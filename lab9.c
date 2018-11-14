ccca380f3a5b23ff06e40718a0b0208a5fcc5d16
Braeden Z Trautz <tui99577@astro.ocis.temple.edu>
2018-11-14 18:44:22 -0500

    lab9 fourth commit

---
+++ lab9.c
@@ -0,0 +1,37 @@
+// Function Comment Template:
+
+/*
+ * Function:	*
+ * Programmer: 	Braeden Trautz
+ * Date:	*
+ * Inputs:	*
+ * Outputs:	*
+ * Globals:	*
+ * Returns:	*
+ * Description:	*
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * Location: 	Temple University Computer Science
+ * Programmer: 	Braeden Z. Trautz
+ * Class: 	Introduction to C Programming 1057 Fall 2018
+ * Assignment:	*
+ * Date:	*
+ * Version: 	1
+ * Description:	*
+ */
+
+//function prototypes
+//preprocessor directives (#define)
+//global constants
+
+int main ( )
+{
+	return EXIT_SUCCESS;
+}
+
+
+//function definitions
