dc3928c9c19924c65bb74d93313d6b161b6f23dc
Braeden Trautz <braedentrautz@gmail.com>
2018-11-28 12:49:00 -0500

    lab 9 full version

--- lab9.c
+++ lab9.c
@@ -1,5 +1,4 @@
 // Function Comment Template:
-
 /*
  * Function:	*
  * Programmer: 	Braeden Trautz
@@ -11,27 +10,101 @@
  * Description:	*
  */
 
-#include <stdio.h>
-#include <stdlib.h>
+#include "../include/lab9Header.h"
 
 /*
  * Location: 	Temple University Computer Science
  * Programmer: 	Braeden Z. Trautz
  * Class: 	Introduction to C Programming 1057 Fall 2018
- * Assignment:	*
- * Date:	*
- * Version: 	1
- * Description:	*
+ * Assignment:	Lab 9 - CSV File Creator
+ * Date:	27 November 2018
+ * Version: 	2
+ * Description:	Combines the omr.txt and names.txt files into one full data file
  */
 
-//function prototypes
-//preprocessor directives (#define)
-//global constants
+char *findName( int a );
+double findGrade( char *choices, char *key );
+
+#define NUM_QS 11
+#define NAME_LEN 50
 
 int main ( )
 {
+
+	FILE *FileA = fopen("omr.txt", "r");
+	FILE *Out = fopen("full_data.csv", "w");
+
+	int id = 0;
+	int i = 0;
+	char choices[NUM_QS];
+	char key[NUM_QS];
+	char name[NAME_LEN];
+	double grade;
+
+	fscanf( FileA, "%d", &id );
+	fprintf( Out, "%5.5u,", id );
+	fprintf( Out, "%s,", findName( id ) );
+	fscanf( FileA, "%s", key );
+	for ( i = 0; i < NUM_QS; i++ )
+		fprintf( Out, "%c,", key[i] );
+	fprintf( Out, "100%\n" );
+
+	while ( fscanf( FileA, "%d", &id ) == 1 ){
+
+		fprintf( Out,"%5.5u,", id);
+		fprintf( Out, "%s,", findName(id));
+		fscanf( FileA, "%s", choices );
+		for ( i = 0; i < NUM_QS; i++ )
+			fprintf( Out, "%c,", choices[i] );
+		fprintf( Out, "%3.2lf%%", findGrade(choices, key) );
+		fprintf( Out, "\n" );
+	}
+
+	fclose(Out);
 	return EXIT_SUCCESS;
+
 }
 
+char *findName ( int a )
+{
 
-//function definitions
+	int id, flag = 0;
+	char s[NAME_LEN], s2[NAME_LEN];
+	FILE *FileB = fopen("names.txt", "r");
+
+	while ( fscanf( FileB, "%d", &id ) == 1 ){
+
+		fscanf( FileB, "%s", &s );
+		fscanf( FileB, "%s", &s2 );
+		strcat( s, " " );
+		strcat( s, s2 );
+
+		if ( id == a ){
+
+			flag = 1;
+			break;
+
+		}
+	}
+
+	return ( flag == 1 ) ? s : "Error!";
+
+}
+
+double findGrade ( char *choices, char *key )
+{
+
+	int i;
+	int numRight = NUM_QS;
+	double grade = 0;
+
+	for ( i = 0; i < NUM_QS; i++ ) {
+		if ( choices[i] != key[i] )
+			numRight--;
+	}
+	grade = (double) numRight / (double) NUM_QS;
+	grade = grade * 100;
+//	printf("Grade: %5.2lf\n", grade);
+	return grade;
+
+}
